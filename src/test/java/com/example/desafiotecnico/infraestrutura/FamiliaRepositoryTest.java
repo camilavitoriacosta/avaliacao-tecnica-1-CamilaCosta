1dc1356b05e22985971a657255914d6a0c3cb21c
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2023-05-22 14:39:56 -0400

    testeRepository

--- src/test/java/com/example/desafiotecnico/infraestrutura/FamiliaRepositoryTest.java
+++ src/test/java/com/example/desafiotecnico/infraestrutura/FamiliaRepositoryTest.java
@@ -1,56 +1,56 @@
-// package com.example.desafiotecnico.infraestrutura;
+package com.example.desafiotecnico.infraestrutura;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-// import java.text.ParseException;
-// import java.util.ArrayList;
-// import java.util.List;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-// import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
 
-// import com.example.desafiotecnico.builders.FamiliaBuilder;
-// import com.example.desafiotecnico.dominio.entidades.Familia;
+import com.example.desafiotecnico.builders.FamiliaBuilder;
+import com.example.desafiotecnico.dominio.entidades.Familia;
 
-// @DataJpaTest
-// @Transactional
-// public class FamiliaRepositoryTest {
-//     @Autowired
-//     private FamiliaRepository familiaRepository;
+@DataJpaTest
+@Transactional
+public class FamiliaRepositoryTest {
+    @Autowired
+    private FamiliaRepository familiaRepository;
 
-//     @Test
-//     void deve_cadastrar_uma_familia() throws ParseException {
-//         Familia familia = new FamiliaBuilder().construir();
+    @Test
+    void deve_cadastrar_uma_familia() throws ParseException {
+        Familia familia = new FamiliaBuilder().construir();
 
-//         familiaRepository.save(familia);
+        familiaRepository.save(familia);
 
-//         assertNotNull(familia.getId());
-//     }
+        assertNotNull(familia.getId());
+    }
 
-//     @Test
-//     void deve_buscar_familias_ordenadas_por_pontuacao() throws ParseException {
-//         Familia familia2 = new FamiliaBuilder().comPontuacao(3).construir();
-//         familiaRepository.save(familia2);
+    @Test
+    void deve_buscar_familias_ordenadas_por_pontuacao() throws ParseException {
+        Familia familia2 = new FamiliaBuilder().comPontuacao(3).construir();
+        familiaRepository.save(familia2);
 
-//         Familia familia1 = new FamiliaBuilder().comPontuacao(5).construir();
-//         familiaRepository.save(familia1);
+        Familia familia1 = new FamiliaBuilder().comPontuacao(5).construir();
+        familiaRepository.save(familia1);
 
-//         List<Familia> familiasOrdenadasEsperadas = new ArrayList<>();
-//         familiasOrdenadasEsperadas.add(familia1);
-//         familiasOrdenadasEsperadas.add(familia2);
+        List<Familia> familiasOrdenadasEsperadas = new ArrayList<>();
+        familiasOrdenadasEsperadas.add(familia1);
+        familiasOrdenadasEsperadas.add(familia2);
 
-//         List<Familia> familiasOrdenadas = familiaRepository.findAllOrderByPontuacao();
+        List<Familia> familiasOrdenadas = familiaRepository.findAllOrderByPontuacao();
 
-//         assertEquals(familiasOrdenadasEsperadas.size(), familiasOrdenadas.size());
+        assertEquals(familiasOrdenadasEsperadas.size(), familiasOrdenadas.size());
 
-//         for (int i = 0; i < familiasOrdenadasEsperadas.size(); i++) {
-//             Familia familiaEsperada = familiasOrdenadasEsperadas.get(i);
-//             Familia familiaAtual = familiasOrdenadas.get(i);
+        for (int i = 0; i < familiasOrdenadasEsperadas.size(); i++) {
+            Familia familiaEsperada = familiasOrdenadasEsperadas.get(i);
+            Familia familiaAtual = familiasOrdenadas.get(i);
 
-//             assertEquals(familiaEsperada.getPontuacao(), familiaAtual.getPontuacao());
-//         }
-//     }
-// }
+            assertEquals(familiaEsperada.getPontuacao(), familiaAtual.getPontuacao());
+        }
+    }
+}
